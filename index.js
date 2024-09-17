6da04bf95615870b8d95c4081291ad17ea561c73
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-16 20:10:39 -0500

    added mathjs code, and installed babel

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const moment = require('moment');
+import {sqrt} from 'mathjs';
+
 console.log("Hello from js");
 
 var datetime = moment().startOf("day").fromNow();
 console.log(datetime);
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+console.log(sqrt(-4).toString()) // 2i
